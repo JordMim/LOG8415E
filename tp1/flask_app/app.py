09c579602f0603d6bf43c1fba507b6b831a498f6
Antoine Lombardo <lombardoa@oxoc.net>
2022-10-06 11:07:09 -0400

    Created a basic Flask app

---
+++ tp1/flask_app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(message)s')
+
+# Create Flask app
+app = Flask(__name__)
+
+# Hello World route
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+# Start in production mode
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=80)
