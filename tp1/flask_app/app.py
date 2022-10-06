dc419264c39ef6d649f252b06db2df10b780d6ff
Antoine Lombardo <lombardoa@oxoc.net>
2022-10-06 13:22:06 -0400

    cluster routes now return the instance ID.

--- tp1/flask_app/app.py
+++ tp1/flask_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import logging, requests
+import logging, requests, json
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
@@ -11,18 +11,21 @@ except:
     INSTANCE_ID = 'unknown'
 logging.info(f'Instance ID: {INSTANCE_ID}')
 
+# Prepare response
+CLUSTER_RESPONSE = json.dumps({'instance_id': INSTANCE_ID})
+
 # Create Flask app
 app = Flask(__name__)
 
 # Cluster 1
 @app.route('/cluster1')
 def cluster_1():
-    return 'Hello World'
+    return CLUSTER_RESPONSE
 
 # Cluster2
 @app.route('/cluster2')
 def cluster_2():
-    return 'Hello World'
+    return CLUSTER_RESPONSE
 
 # Ping
 @app.route('/ping')
