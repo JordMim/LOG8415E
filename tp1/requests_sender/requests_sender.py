a89f47e2f7bc7402bf9c2224c91a48dd5cf762db
Jordan Mimeault <jordan.mimeault@polymtl.ca>
2022-10-06 14:15:00 -0400

    Updated the requests sender.

--- tp1/requests_sender/requests_sender.py
+++ tp1/requests_sender/requests_sender.py
@@ -1,33 +1,37 @@
 import json, requests, threading, time, logging
 
+BASE_URL = 'http://tp1-1158016110.us-east-1.elb.amazonaws.com'
+
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
 
-def call_endpoint_http():
-    url = 'http://localhost'
+def call_endpoint_http(url):
     response = requests.get(url)
 
-def thread_1_fct():
+def thread_1_fct(url):
     for i in range(1000):
-        call_endpoint_http()
-    logging.info('Thread 1 ended.')
+        call_endpoint_http(url)
+    logging.info('-> Thread 1 ended.')
 
-def thread_2_fct():
+def thread_2_fct(url):
     for i in range(500):
-        call_endpoint_http()
+        call_endpoint_http(url)
     time.sleep(60)
     for i in range(1000):
-        call_endpoint_http()
-    logging.info('Thread 2 ended.')
+        call_endpoint_http(url)
+    logging.info('-> Thread 2 ended.')
 
 if __name__ == "__main__":
 
-    thread_1 = threading.Thread(target=thread_1_fct)
-    thread_2 = threading.Thread(target=thread_2_fct)
-    logging.info('Running thread 1...')
-    thread_1.start()
-    logging.info('Running thread 2...')
-    thread_2.start()
-    thread_1.join()
-    thread_2.join()
-    logging.info('Program ended.')
+    for cluster in ('cluster1', 'cluster2'):
+        logging.info('Sending requests to ' + cluster)
+        url = BASE_URL + '/' + cluster
+        thread_1 = threading.Thread(target=thread_1_fct, args=(url,))
+        thread_2 = threading.Thread(target=thread_2_fct, args=(url,))
+        logging.info('-> Running thread 1...')
+        thread_1.start()
+        logging.info('-> Running thread 2...')
+        thread_2.start()
+        thread_1.join()
+        thread_2.join()
+        logging.info('-> Ended.')
