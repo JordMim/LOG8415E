65fbb292f93aa906f2ec9b6e19ff7961ac36c653
Antoine Lombardo <lombardoa@oxoc.net>
2022-10-06 13:38:50 -0400

    Created a startup script for launching the Flask app.

---
+++ tp1/flask_app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+yum update -y
+yum install git tmux htop -y
+cd ~
+git clone https://github.com/JordMim/LOG8415E.git
+cd LOG8415E
+git pull
+cd tp1/flask_app
+pip3 install -r requirements.txt
+tmux kill-session -t flask
+tmux new-session -d -s flask
+tmux send-keys 'python3 app.py' C-m
+tmux detach -s flask
