a4719faf7e1ca6da134b67c83de169eb9b0dcd85
Alexandre Dufort <Dufort.Alexandre1@gmail.com>
2022-10-06 13:10:21 -0400

    Switched from 0.0.0.0 to 127.0.0.1.

--- tp1/flask_app/app.py
+++ tp1/flask_app/app.py
@@ -32,4 +32,4 @@ def ping():
 # Start in production mode
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=80)
+    serve(app, host="127.0.0.1", port=80)
