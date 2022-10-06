a6fc2c2c73b76a3d79af761084d648408e77e278
Alexandre Dufort <Dufort.Alexandre1@gmail.com>
2022-10-06 13:05:28 -0400

    Added cluster1 and cluster2 routes.

--- tp1/flask_app/app.py
+++ tp1/flask_app/app.py
@@ -1,16 +1,33 @@
 from flask import Flask
-import logging
+import logging, requests
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
 
+# Get instance ID
+try:
+    INSTANCE_ID = requests.get('http://169.254.169.254/latest/meta-data/instance-id').text
+except:
+    INSTANCE_ID = 'unknown'
+logging.info(f'Instance ID: {INSTANCE_ID}')
+
 # Create Flask app
 app = Flask(__name__)
 
-# Hello World route
-@app.route('/')
-def hello_world():
+# Cluster 1
+@app.route('/cluster1')
+def cluster_1():
     return 'Hello World'
+
+# Cluster2
+@app.route('/cluster2')
+def cluster_2():
+    return 'Hello World'
+
+# Ping
+@app.route('/ping')
+def ping():
+    return 'pong'
 
 # Start in production mode
 if __name__ == "__main__":
