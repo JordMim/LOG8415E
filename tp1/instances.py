06aaec10af50e2eb798e9e8e3e889bdb93c90d50
Jacob Dupuis <jacobdu99@outlook.com>
2022-10-16 17:58:33 -0400

    main.py is now able to completely create the system.

--- tp1/instances.py
+++ tp1/instances.py
@@ -1,18 +1,46 @@
-import boto3, os
-from boto3_type_annotations.ec2 import Client, ServiceResource
+import boto3, os, logging
+from typing import List
+from boto3_type_annotations.ec2 import ServiceResource, SecurityGroup, Instance
 
 USER_DATA_SCRIPT_FILE = 'instance_user_data.txt'
 dir_path = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(dir_path, USER_DATA_SCRIPT_FILE)) as file:
     USER_DATA_SCRIPT = file.read()
 
-def create_instance(ec2: ServiceResource, tag, instance_type):
+def delete_all_instances(ec2: ServiceResource):
+    logging.info('Terminating all instances...')
+    for instance in ec2.instances.all():
+        instance.terminate()
+    for instance in ec2.instances.all():
+        logging.info('  {}: Terminated.'.format(instance.id))
+        instance.wait_until_terminated()
 
-    ec2.create_instances(
+
+def create_instance(ec2: ServiceResource, instance_type: str, availability_zone: str, security_group: SecurityGroup) -> Instance:
+    instance: Instance = ec2.create_instances(
         ImageId='ami-026b57f3c383c2eec',
         MinCount=1,
         MaxCount=1,
         InstanceType=instance_type,
         UserData=USER_DATA_SCRIPT,
-        KeyName='vockey'
-    )
+        KeyName='vockey',
+        Placement={
+            'AvailabilityZone': availability_zone,
+        }
+    )[0]
+    return instance
+
+def wait_for_running(instances: List[Instance]):
+    logging.info('Waiting for all instances to be running...')
+    for instance in instances:
+        instance.wait_until_running()
+        logging.info('  {}: Running.'.format(instance.id))
+
+def retreive_instances(ec2: ServiceResource, instance_type: str) -> List[Instance]:
+    instances = []
+    for instance in ec2.instances.all():
+        if instance.state['Name'] == 'terminated':
+            continue
+        if instance.instance_type == instance_type:
+            instances.append(instance)
+    return instances
--- tp1/security_groups.py
+++ tp1/security_groups.py
@@ -2,12 +2,17 @@ from typing import Collection
 import boto3, os
 from boto3_type_annotations.ec2 import ServiceResource, SecurityGroup
 
-def delete_security_group(ec2: ServiceResource, name: str):
+def get_security_group(ec2: ServiceResource, name: str):
     security_groups: Collection[SecurityGroup] = ec2.security_groups.all()
     for security_group in security_groups:
         if security_group.group_name == name:
-            security_group.delete()
-            return
+            return security_group
+    return None
+
+def delete_security_group(ec2: ServiceResource, name: str):
+    security_group = get_security_group(ec2, name)
+    if security_group is not None:
+        security_group.delete()
 
 def create_security_group(ec2: ServiceResource, name: str) -> SecurityGroup:
     security_group: SecurityGroup = ec2.create_security_group(
